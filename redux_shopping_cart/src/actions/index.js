50839106720b30ed6e3791ff1918eb827de1b76d
iamsuneeth <suneeth.lenin@gmail.com>
2017-06-05 21:05:03 +0530

    added thunk middleware

--- redux_shopping_cart/src/actions/index.js
+++ redux_shopping_cart/src/actions/index.js
@@ -1,11 +1,25 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 
-export const addToCart = (item) => ({
+/*export const addToCart = (item) => ({
     type: ADD_TO_CART,
     payload: item
-})
+})*/
 
+export const addToCart = (item) => {
+    return (dispatch, getState) => {
+        const reduxState = getState();
+        const itemExists = reduxState.shoppingCart.filter((cartItem) => (
+            cartItem.id === item.id
+        ));
+        if(itemExists.length===0){
+            dispatch({
+                type: ADD_TO_CART,
+                payload: item 
+            })
+        }
+    }
+}
 export const removeFromCart = (item) => ({
     type:REMOVE_FROM_CART,
     payload: item
